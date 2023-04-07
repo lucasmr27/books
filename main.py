e359907b8cd9270429ee05174ab37207dd0f748f
Lucas Rocha <lucas_rocha27@hotmail.com>
2023-04-07 01:08:46 -0300

    Extração dos links de cada livro concluida

---
+++ main.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+import time
+
+url = "http://books.toscrape.com/catalogue/page-48.html"
+
+driver = webdriver.Firefox()
+driver.get(url)
+
+soup = BeautifulSoup(driver.page_source, "html.parser")
+ultima_pagina = False
+while(not ultima_pagina):
+    try:
+        # Extrai todos os links da página
+        for heading in soup.select('h3'):
+            for link in heading.select('a'):
+                print(link['href'])
+
+        # encontrar o elemento "next" pelo link de texto
+        next_link = driver.find_element(By.LINK_TEXT, 'next')
+
+        # obter o valor do atributo href do elemento "next"
+        next_url = next_link.get_attribute("href")
+
+        # navegar para a próxima página
+        driver.get(next_url)
+
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+    except:
+        ultima_pagina = True
+
+print("#################   saiu   #######################")
