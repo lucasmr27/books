1723ed52fd68247664331ec6338f3ecbcbd09e90
Lucas Rocha <lucas_rocha27@hotmail.com>
2023-04-12 06:06:36 -0300

    melhorias no codigo, notas salvas como inteiro

--- dados.py
+++ dados.py
@@ -22,7 +22,18 @@ with open('arquivos/dados.csv', mode='w', newline='') as file:
         soup = BeautifulSoup(driver.page_source, "html.parser")
         dados.append(soup.find("div", class_="col-sm-6 product_main").h1.string)  # Título do livro
         dados.append(soup.select_one("ul.breadcrumb > li:nth-of-type(3) > a").text) # Categoria do livro
-        dados.append(soup("p")[2]['class'][1])  # Nota do livro
+        nota = soup("p")[2]['class'][1]  # Nota do livro
+
+        notas_dict = {
+            'One': 1,
+            'Two': 2,
+            'Three': 3,
+            'Four': 4,
+            'Five': 5
+        }
+        
+        dados.append(notas_dict[nota])
+
         dados += [dado.text for dado in soup.select("tr > td")]  # Informações do livro em uma lista
 
         writer.writerow(dados)  # Salva os dados no arquivo CSV
