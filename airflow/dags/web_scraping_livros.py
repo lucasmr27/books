adfbd629e7a6b1743f10b9e02a1fc5a121528bac
lucasmr27 <lucas_rocha27@hotmail.com>
2023-04-14 10:00:05 -0300

    aplicação do airflow

--- airflow/dags/web_scraping_livros.py
+++ airflow/dags/web_scraping_livros.py
@@ -10,19 +10,19 @@ with DAG('scraping_books', start_date=datetime(2023, 4, 9),
     # Define a primeira tarefa, que será responsável por capturar os links dos livros a serem raspados.
     captura_links = PythonOperator(
         task_id='captura_links',
-        python_callable=exec(open('C:/Users/lucas/OneDrive/Documentos/Estudos/books/captura_links.py').read())
+        python_callable=exec(open('/home/lucas/projetos/books/captura_links.py').read())
     )
 
     # Define a segunda tarefa, que será responsável por extrair os dados dos livros a partir dos links coletados.
     extrai_dados = PythonOperator(
         task_id='extrai_dados',
-        python_callable=exec(open('C:/Users/lucas/OneDrive/Documentos/Estudos/books/extrai_dados.py').read())
+        python_callable=exec(open('/home/lucas/projetos/books/extrai_dados.py').read())
     )
 
     # Define a terceira tarefa, que será responsável por inserir os dados dos livros no banco de dados.
     insere_bd = PythonOperator(
         task_id='insere_bd',
-        python_callable=exec(open('C:/Users/lucas/OneDrive/Documentos/Estudos/books/insere_bd.py').read())
+        python_callable=exec(open('/home/lucas/projetos/books/insere_bd.py').read())
     )
     
     # Define que as tarefas devem ser executadas em sequência, na ordem em que foram definidas.
